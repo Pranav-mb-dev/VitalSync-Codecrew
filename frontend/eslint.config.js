d3b89f76da981158b25d559911b80cffbb9f1333
Venkatachalam S <venkatachalamsubramanian23@gmail.com>
2026-03-29 09:47:35 +0530

    Fix ESLint plugin Vercel warning and bypass lint failing the build temporarily

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,16 +1,19 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import nextPlugin from "@next/eslint-plugin-next";
+import reactPlugin from "eslint-plugin-react";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ['dist', '.next'] },
+  js.configs.recommended,
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-    ],
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+      '@next/next': nextPlugin,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: {
@@ -24,10 +27,12 @@ export default defineConfig([
       },
     },
     rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
       'no-unused-vars': 'off',
       'no-empty': 'off',
       'react-hooks/exhaustive-deps': 'off',
-      'react-hooks/set-state-in-effect': 'off',
     },
   },
-])
+];
